93d1753efe186d0dfde077e458cecfcdb4de0472
boilnkettle <jmiguelsramos@gmail.com>
2019-05-29 23:31:14 +0100

    Add explanations for wrong behaviour of AArrayStackV1.java & AArrayStackV2.java

--- src/pc/stack/AArrayStackV1.java
+++ src/pc/stack/AArrayStackV1.java
@@ -29,6 +29,23 @@ public class AArrayStackV1<E> implements Stack<E> {
 
   @Override
   public void push(E elem) {
+    /* Qualquer uma das instrucoes seguintes e atomica:
+     * - verificar se elem e null;
+     * - atualizar o valor do top usando getAndIncrement();
+     * - e colocar elem no array.
+     * Mas o conjunto destas nao o e.
+     *
+     * Para corrigir este comportamento, podemos colocar o
+     * metodo como synchronized, mas isto alteraria a 
+     * implementacao para uma forma bloqueante.
+     * A melhor maneira de corrigir este comportamento, mas
+     * mantendo a forma nao-bloqueante seria alterando a
+     * implementacao de modo a termos um estado que combina
+     * o topo da stack e o seu tamanho num so e desse modo
+     * atualizar o topo da stack seria actualizar o seu 
+     * estado de forma atomica usando por exemplo a instrucao
+     * compareAndSet() como na aula de laboratorio 3
+     */
     if (elem == null) {
       throw new IllegalArgumentException();
     }
@@ -38,6 +55,7 @@ public class AArrayStackV1<E> implements Stack<E> {
 
   @Override
   public E pop() {
+    /* O que foi descrito no metodo push() tambem se aplica aqui. */
     if (top.get() == 0) {
       return null;
     }
